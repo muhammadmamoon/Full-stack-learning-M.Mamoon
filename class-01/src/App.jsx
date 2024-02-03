6b1ca86993849bca2d3778c161b668b0cd709e17
Muhammad Mamoon <muhammadmamoon771@gmail.com>
2024-02-04 00:42:03 +0500

    vit project

---
+++ class-01/src/App.jsx
@@ -0,0 +1,20 @@
+import react from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <div>
+      <h1>Muhammad Mamoon</h1>
+      <p>Learning Full stack Developnment</p>
+    </div>
+    </>
+      
+  )
+}
+
+export default App
